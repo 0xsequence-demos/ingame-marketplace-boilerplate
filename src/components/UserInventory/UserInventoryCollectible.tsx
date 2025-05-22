6f949272b477e071fe5b730aa3824979510597ed
Tomasz Dysinski <dysinski.tomasz@gmail.com>
2025-05-21 23:34:42 -0700

    label changes and cleanup

--- src/components/UserInventory/UserInventoryCollectible.tsx
+++ src/components/UserInventory/UserInventoryCollectible.tsx
@@ -106,7 +106,7 @@ export const UserInventoryCollectible = ({
               className="py-3 px-3 border border-transparent bg-[linear-gradient(to_left,_#7537f9,_#5826ff)] rounded-[0.5rem] min-w-[50px] font-bold text-14 cursor-pointer"
               onClick={onClickList}
             >
-              List
+              List for Sale
             </button>
           )}
           {!sellDisabled && (
